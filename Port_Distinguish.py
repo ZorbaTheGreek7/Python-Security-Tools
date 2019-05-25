47f5f8e8c2a16e52c3a373e5ce51edd6426d1662
ZorbaTheGreek7 <50997879+ZorbaTheGreek7@users.noreply.github.com>
2019-05-25 22:20:27 +0200

    Add files via upload

---
+++ Port_Distinguish.py
@@ -0,0 +1,38 @@
+import sys
+import textwrap
+import setuptools
+
+
+def main():
+    requires = []
+    scripts = []
+    py_version = sys.version_info[:2]
+    if py_version < (3, 3):
+        requires.append('mock(>=1.0)')
+    if py_version == (3, 3):
+        requires.append('omar(>=3.4)')
+    if py_version >= (3, 3):
+        scripts.append('src/portserver.py')
+
+    setuptools.setup(
+        name='port_distinguisher',
+        version='1.3.1',
+        description='A simple code to choose unique available network ports.',
+        long_description=textwrap.dedent("""\
+          port_distinguisher provides an API to find and return an available network
+          port for an application to bind to. Ideally suited for use from
+          unittests or for test harnesses that launch local servers."""),
+        license='Apache 2.0',
+        maintainer='Google',
+        maintainer_email='omarelnahal7@gmail.com',
+        url='https://github.com/ZorbaTheGreek7/Python-Security-Tools/blob/master/Port_Distinguish.py',
+        package_dir={'': 'src'},
+        py_modules=['port_distinguisher'],
+        platforms=['POSIX'],
+        requires=requires,
+        scripts=scripts,
+        )
+
+
+if __name__ == '__main__':
+    main()
